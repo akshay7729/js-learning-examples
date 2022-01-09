22071c84ed4e4f2fc2ed65e2bb9a9c7a05e54c74
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-01-09 21:02:14 +0530

    For in and normal for loop

---
+++ Many types of FOR loops/for-in.js
@@ -0,0 +1,37 @@
+// for in loop iterates over properties of an object
+// for in will loop through property name, index
+
+const obj = {
+  pet: "Dog",
+  name: "Cheeseburger",
+  breed: "English Bulldog",
+  age: 4,
+};
+
+const arr = [1, 5, 3, 2, 5, 6, 2, 7, 9, 4];
+
+// with object
+for (let key in obj) {
+  // will give key name or index
+  // output -
+  //   pet
+  //   name
+  //   breed
+  //   age
+  console.log("key", key);
+
+  // will give values
+  // output -
+  //    Dog
+  //    Cheeseburger
+  //    English Bulldog
+  //    4
+  console.log("val", obj[key]);
+}
+
+// array
+for (let key in arr) {
+  console.log("arr key", key);
+
+  console.log("arr val", arr[key]);
+}
---
+++ Many types of FOR loops/normal-for-loop.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 5, 3, 7, 2, 8, 4];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
