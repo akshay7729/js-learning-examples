4ab914c1d0832f8970898ba030279c831d84a1f3
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-01-09 22:32:55 +0530

    Apply.js

--- call Bind and Apply.js
+++ call Bind and Apply.js
@@ -1,37 +1,37 @@
 var akshay = {
-    name: 'Akshay',
-    age: 28,
-    job: 'developer',
-    presentation: function(style, timeOfDay) {
-        if(style === 'formal'){
-            console.log(`good ${timeOfDay}. 
+  name: "Akshay",
+  age: 28,
+  job: "developer",
+  presentation: function (style, timeOfDay) {
+    if (style === "formal") {
+      console.log(`good ${timeOfDay}. 
                             I am ${this.name}, 
                             I am ${this.age} years old
                             I am a ${this.job}
                             `);
-        }else if(style === "casual"){
-            console.log(`Hey what's up.
+    } else if (style === "casual") {
+      console.log(`Hey what's up.
                         I am ${this.name}, 
                         I am ${this.age} years old
                         I am a ${this.job}  
                         Have a nice ${timeOfDay}   
-            `)
-        }
+            `);
     }
-}
+  },
+};
 
 var rinku = {
-    name: 'Rinku',
-    age: 26,
-    job: 'developer'
-}
+  name: "Rinku",
+  age: 26,
+  job: "developer",
+};
 
-akshay.presentation('formal','morning');
+akshay.presentation("formal", "morning");
 
-// call method in action 
+// call method in action
 // rinku object is switched with this paramater.
 // So, now akshay object is switched with rinku
-akshay.presentation.call(rinku, 'formal', 'morning');
+akshay.presentation.call(rinku, "formal", "morning");
 
 // apply takes arguments as an array
 // akshay.presentation.apply(rinku, ['formal','morning']);
@@ -39,6 +39,10 @@ akshay.presentation.call(rinku, 'formal', 'morning');
 // bind allows us to preset an argument
 // bind makes a copy of the function and does not call immediately
 // works like immutable variables
-var akshayWork = akshay.presentation.bind(rinku,'casual');
-akshayWork('afternoon');
-akshayWork('evening');
+var akshayWork = akshay.presentation.bind(rinku, "casual");
+akshayWork("afternoon");
+akshayWork("evening");
+
+// apply takes as an argument
+// it is similar to call, but takes arguments as an array
+akshay.presentation.apply(rinku, ["casual", "morning"]);
