f8b545fdc84f98c67067eff01414577be39314e2
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-02-10 09:19:48 +0530

    flat array

---
+++ Arrays/flatten-an-array.js
@@ -0,0 +1,17 @@
+// input ->  [1, 2, [3, 4], 5, [[[6, 7], 8, [[[[9]]]]]]]
+// output -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// solution 1
+const flatArray = (arr) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(flatArray([1, 2, [3, 4], 5, [[[6, 7], 8, [[[[9]]]]]]]));
